8677376d0ddaf7d8d9f931cf7626c6da30b1206b
ers1804 <80150625+ers1804@users.noreply.github.com>
2021-12-08 17:40:01 +0100

    Changed parameters for experiment_3

--- scripts/train.py
+++ scripts/train.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
     #################################### [start] scanrefer arguments #######################################
     parser.add_argument("--tag", type=str, help="tag for the training, e.g. cuda_wl", default="")
     parser.add_argument("--gpu", type=str, help="gpu", default="0")
-    parser.add_argument("--batch_size", type=int, help="batch size", default=8)
+    parser.add_argument("--batch_size", type=int, help="batch size", default=10)
     parser.add_argument("--epoch", type=int, help="number of epochs", default=5000)
     parser.add_argument("--verbose", type=int, help="iterations of showing verbose", default=10)
     parser.add_argument("--val_step", type=int, help="iterations of validating", default=20)
