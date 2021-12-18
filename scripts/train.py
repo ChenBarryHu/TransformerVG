119b7585fdf60e5bdadecb46eef8852ac43be1c5
ers1804 <80150625+ers1804@users.noreply.github.com>
2021-12-18 10:19:46 +0100

    Changed arguments in train for RTX2080Ti.

--- scripts/train.py
+++ scripts/train.py
@@ -269,9 +269,9 @@ if __name__ == "__main__":
     parser.add_argument("--tag", type=str, help="tag for the training, e.g. cuda_wl", default="")
     parser.add_argument("--gpu", type=str, help="gpu", default="0")
     parser.add_argument("--batch_size", type=int, help="batch size", default=5)
-    parser.add_argument("--epoch", type=int, help="number of epochs", default=5000)
-    parser.add_argument("--verbose", type=int, help="iterations of showing verbose", default=10)
-    parser.add_argument("--val_step", type=int, help="iterations of validating", default=5000)
+    parser.add_argument("--epoch", type=int, help="number of epochs", default=10)
+    parser.add_argument("--verbose", type=int, help="iterations of showing verbose", default=2)
+    parser.add_argument("--val_step", type=int, help="iterations of validating", default=2)
     parser.add_argument("--lr", type=float, help="learning rate", default=1e-3) # 1e-3 is a better lr in the experiment so far
     parser.add_argument("--wd", type=float, help="weight decay", default=1e-5)
     parser.add_argument("--num_points", type=int, default=40000, help="Point Number [default: 40000]")
